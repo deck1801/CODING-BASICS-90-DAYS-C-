e045e855c06a2258b7a3e58687493d0372aef3ae
Divyajyoti <divyajyoti182000@gmail.com>
2022-01-04 22:26:47 +0530

    Intial Commit Start of REGAINING

---
+++ CheckingSetup.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"SETUP SUCCCESSFULL"<<"\n";
+    return 0;
+}
---
+++ prtal1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"Size of Integer: "<<sizeof(a)<<"\n";
+    float b;
+    cout<<"Size of Float: "<<sizeof(b)<<"\n";
+    double c;
+    cout<<"Size of Double: "<<sizeof(c)<<"\n";
+    char d;
+    cout<<"Size of Character: "<<sizeof(d)<<"\n";
+    bool e;
+    cout<<"Size of Boolean: "<<sizeof(e)<<"\n";
+    short int si;
+    cout<<"Size of Short int: "<<sizeof(si)<<"\n";
+    long int li;
+    cout<<"Size of Long int: "<<sizeof(li)<<"\n";
+    return 0;
+}
