06d17d76c41c1e89bafd61a087c06bacfbae2e43
Divyajyoti <divyajyoti182000@gmail.com>
2022-01-31 16:58:47 +0530

    Patter Practice Section-1 Compeleted

--- prtal7.cpp
+++ prtal7.cpp
@@ -32,6 +32,78 @@ int main(){
         cout<<"\n";
      }
      
+     cout<<"\n";
+     cout<<"PATTERN-3: "<<"\n";
+     //pattern-3
+     for(int i=6;i>0;i--){
+         for(int j=0;j<i;j++){
+            cout<<"*"<<" ";
+         }
+         cout<<"\n";
+     }
+
+     cout<<"\n";
+     cout<<"PATTERN-4: "<<"\n";
+     //pattern-4
+     for(int i=0;i<6;i++){
+         for(int j=i;j>0;j--){
+            cout<<"*"<<" ";
+         }
+         cout<<"\n";
+     }
+
+     cout<<"\n";
+     cout<<"PATTERN-5: "<<"\n";
+     //pattern-5
+     for(int i=0;i<6;i++){
+         for(int j=i;j>0;j--){
+            cout<<i<<" ";
+         }
+         cout<<"\n";
+     }
+    
+    cout<<"\n";
+    cout<<"PATTERN-5: "<<"\n";
+    //pattern-5
+    int count=1;
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=i;j++){
+            cout<<count<<" ";
+            count++;
+        }
+        cout<<"\n";
+    }
+
+    cout<<"\n";
+    cout<<"PATTERN-6: "<<"\n";
+    //pattern-6
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*"<<" ";
+        }
+        int space=(2*5)-(2*i);
+        for(int j=1;j<=space;j++){
+            cout<<" "<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<"*"<<" ";
+        }
+        cout<<"\n";
+     }
+     for(int i=5;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            cout<<"*"<<" ";
+        }
+        int space=(2*5)-(2*i);
+        for(int j=1;j<=space;j++){
+            cout<<" "<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<"*"<<" ";
+        }
+        cout<<"\n";
+     }
+
     
     
     return 0;
