ea0753a1353e1e6646f19a8f3f7d3c0fdea5bfdd
Divyajyoti <divyajyoti182000@gmail.com>
2022-02-01 17:57:03 +0530

    Advance pattern practice-2

--- prtal8.cpp
+++ prtal8.cpp
@@ -15,9 +15,47 @@ int main(){
     cout<<"Pattern-2: "<<"\n";
     for(int i=1;i<=5;i++){
         for(int j=1;j<=i;j++){
-            cout<<j-i<<" ";
+            if((i+j)%2==0){
+                cout<<1<<" ";
+            }
+            else{
+                cout<<0<<" ";
+            }
         }
         cout<<"\n";
+    }
+
+    //pattern-3
+    cout<<"Pattern-3"<<"\n";
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=5-i;j++){
+            cout<<" "<<" ";
+        }
+        for(int j=1;j<=5;j++){
+            cout<<"*"<<" ";
+        }
+        cout<<"\n";
+    }
+
+    //pattern-4
+    cout<<"Pattern-4"<<"\n";
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=5-i;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<j<<" ";
+        }
+        cout<<"\n";
+    }
+
+    //pattern-5
+    cout<<"Pattern-5"<<"\n";
+    for(int i=1;i<=9;i++){
+        for(int j=1;j<=9-i;j++){
+            cout<<" ";
+        }
+        
     }
 
 
