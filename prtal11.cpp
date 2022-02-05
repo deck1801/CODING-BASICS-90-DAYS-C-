483ee828861fc58a3b030e5e40b95366abc2fb90
Divyajyoti <divyajyoti182000@gmail.com>
2022-02-05 12:10:05 +0530

    To be Continued

--- prtal11.cpp
+++ prtal11.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<math.h>
+#include<string>
 using namespace std;
 
 //sum of first n natural numbers
@@ -38,12 +39,55 @@ void ConvBtoD(int binary){
     int dec=0;
     while(binary!=0){
         int rev=binary%10;
-        dec+=pow(rev,count);
+        dec+=pow(2,count)*rev;
         count++;
         binary/=10;
     }
     cout<<"Decimal Conversion of Binary: "<<dec<<"\n";
     cout<<"\n";
+}
+
+//Conversion: decimal to Octadecimal / Ocatadecimal to decimal
+void ConvDtoO(int decimal){
+    int octal=0;
+    while(decimal!=0){
+        int rem=decimal%8;
+        octal=octal*10+rem;
+        decimal/=8;
+    }
+    octal=octal*10+decimal;
+    cout<<"Octal conversion of Decimal: "<<octal<<"\n";
+}
+void ConvOtoD(int octal){
+    int dec=0,count=0;
+    while(octal!=0){
+        int rev=octal%10;
+        dec+=pow(8,count)*rev;
+        count++;
+        octal/=10;
+    }
+    cout<<"Decimal conversion of Octal: "<<dec<<"\n";
+    cout<<"\n";
+}
+
+//Conversion: decimal to Hexadecimal / Hexadecimal to decimal
+void ConvDtoH(int decimal){
+    string hexa;
+    while(decimal!=0){
+        int rem=decimal%16;
+        if(rem>=10){
+            hexa+=string(rem+55);
+        }
+        decimal/=16;
+    }
+}
+void ConvHtoD(int hexa){
+
+}
+
+//Sum of Binary Numbers
+void SumOfBin(int bin1,int bin2){
+
 }
 
 
@@ -52,5 +96,7 @@ void ConvBtoD(int binary){
 int main(){
     ConvDtoB(10);
     ConvBtoD(1010);
+    ConvDtoO(32);
+    ConvOtoD(40);
     return 0;
 }
