21908ab3ead41c8e2d49b63c929392b1fc28142e
Divyajyoti <divyajyoti182000@gmail.com>
2022-02-21 22:16:42 +0530

    Object Oriented Programming

--- prtal16.cpp
+++ prtal16.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 using namespace std;
 //OOPS in C++: Object Oriented Programming
 //Class is a blueprint
@@ -27,33 +28,76 @@ class student{
         }
 };
 
+class Profile{
+    string name;
+    int age;
+    bool gender;
+    
+    public:
+    //default constructor only invoked with object creation
+    Profile(){
+        cout<<"There is a data present for students"<<"\n";
+    }
+    //Constructor with perimeters(Parameterized constructor)
+    Profile(string n,int a,bool g){
+        name=n;
+        age=a;
+        gender=g;
+    }
+    //Copy Constructor
+    //deep copy
+    Profile(Profile &s){
+        name=s.name;
+        age=s.age;
+        gender=s.gender;
+    }
 
+    ~Profile(){
+        cout<<"The Destructor has been called"<<"\n";
+    }
+    
+    void display(){
+        cout<<"Name of the student: "<<name<<"\n";
+        cout<<"Age of the student: "<<age<<"\n";
+        cout<<"Gender of the student: "<<gender<<"\n";
+        cout<<"-----------------------------------"<<"\n";
+        cout<<"\n";
+    }
 
-
-
+    //operator overloading: used for instance operation
+    bool operator == (Profile &p){
+        if(name==p.name && age==p.age && gender==p.gender){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
 
 
 int main(){
     //array of objects
     student arr[3];
-    for(int i=0;i<3;i++){
-        int m;
-        cout<<"Enter Name of the student: "<<"\n";
-        cin>>arr[i].name;
-        cout<<"Enter the Age of the student: "<<"\n";
-        cin>>arr[i].age;
-        cout<<"Enter the Gender of the student: "<<"\n";
-        cin>>arr[i].gender;
-        cout<<"Enter the Marks of the student: "<<"\n";
-        cin>>m;
-        arr[i].setMarks(m);
-        cout<<"\n";
+    Profile p; //default constructor invoked
+    Profile p1("Divyajyoti",22,1); //parameterized constructor invoked 
+    p1.display(); //public memeber function
+    Profile p2("Avichal",23,1);
+    p2.display();
+    Profile p3("Priya",22,0);
+    p3.display();
+    Profile c=p1; //Copy consrtuctor will be invoked
+    c.display();
+    if(c==p1){
+        cout<<"The data has been replicated"<<"\n";
+    }else{
+        cout<<"The data not replicated"<<"\n";
     }
-    
-    for(int i=0;i<3;i++){
-        arr[i].PrintFunc();
+    if(p1==p2){
+        cout<<"Data is redundant"<<"\n";
+    }else{
+        cout<<"Data is unique"<<"\n";
     }
-    
 
     return 0;
 }
