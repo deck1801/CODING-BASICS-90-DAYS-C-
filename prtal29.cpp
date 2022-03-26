0817a1d76d981158098b88fe8dc6b5dac4ac3e3f
Divyajyoti <divyajyoti182000@gmail.com>
2022-03-26 19:04:05 +0530

    Bit Manipulation part-3 completed

--- prtal29.cpp
+++ prtal29.cpp
@@ -37,15 +37,40 @@ void FindTwoUniqueNumber(int a[],int n){
     int secxor=temp^newxor;
     cout<<"The first unique: "<<newxor<<"\n";
     cout<<"The second unique: "<<secxor<<"\n";
+}
 
+bool getBit(int n,int pos){
+    return ((n&(1<<pos))!=0);
+}
+
+int setBit(int n,int pos){
+    return (n|(1<<pos));
+}
+
+void FindUniqueInThree(int a[],int n){
+    int result=0;
+    for(int i=0;i<64;i++){
+        int sum=0;
+        for(int j=0;j<n;j++){
+            if(getBit(a[j],i)){
+                sum++;
+            }
+        }
+        if(sum%3!=0){
+            result=setBit(result,i);
+        }
+    }
+    cout<<"The Unique Number: "<<result<<"\n";
 }
 
 
 int main(){
     int arr[7]={2,4,6,3,4,6,2};
     int arr1[8]={2,4,6,7,4,5,6,2};
+    int arr2[10]={1,2,3,3,4,2,1,1,3,2};
     FindUnique(arr,7);
     FindTwoUniqueNumber(arr1,8);
+    FindUniqueInThree(arr2,10);
 
 
     return 0;
